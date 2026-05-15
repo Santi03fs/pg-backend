62319621c429b7fdcf2fc851e0a42995f6b695ce
Santiago <santifernandez652@gmail.com>
2026-05-15 22:47:44 +0200

    AÑADIDO BORRAR TRABAJADOR

--- src/main/java/com/pg/backend/controller/TrabajadorController.java
+++ src/main/java/com/pg/backend/controller/TrabajadorController.java
@@ -3,6 +3,7 @@ package com.pg.backend.controller;
 import com.pg.backend.model.Trabajador;
 import com.pg.backend.repository.TrabajadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,17 @@ public class TrabajadorController {
     @PostMapping
     public Trabajador guardarTrabajador(@RequestBody Trabajador trabajador) {
         return trabajadorRepository.save(trabajador);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminarTrabajador(@PathVariable Long id) {
+        try {
+            trabajadorRepository.deleteById(id);
+            return ResponseEntity.ok().build(); // Todo ha ido bien
+        } catch (Exception e) {
+            // Si falla (seguramente porque el trabajador ya tiene horas en la tabla
+            // asistencias)
+            return ResponseEntity.badRequest().body("No se puede eliminar: El trabajador tiene horas registradas.");
+        }
     }
 }
